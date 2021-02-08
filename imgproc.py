0efa0de4f9fe2df783e798d168314773712f8866
Lorenzo Braccaioli <bracca@Airlol.local>
2021-02-08 22:18:05 +0100

    add four-point transform

--- imgproc.py
+++ imgproc.py
@@ -73,6 +73,33 @@ def cvt2HeatmapImg(img):
     img = cv2.applyColorMap(img, cv2.COLORMAP_JET)
     return img
 
+def four_point_transform(image, pts):
+
+    pts = np.reshape(pts, (4, 2))
+    (tl, tr, br, bl) = pts
+
+    # transformed image new width
+    widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    maxWidth = max(int(widthA), int(widthB))
+    
+    # tranformed image new height
+    heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    maxHeight = max(int(heightA), int(heightB))
+    
+    # TL, TR, BR, BL
+    dst = np.array([
+        [0, 0],
+        [maxWidth - 1, 0],
+        [maxWidth - 1, maxHeight - 1],
+        [0, maxHeight - 1]], dtype=np.float32)
+    
+    # compute the perspective transform matrix and then apply it
+    M = cv2.getPerspectiveTransform(pts, dst)
+    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    
+    return warped
 
 def getAngle(angle):
     '''arg: angle of the original box. Return the angle to make it horizontal'''
